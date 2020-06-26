a839b89f28e1b03a5b1ec765ef51a2aaa42f9ea3
Jere Rantalainen <jere1rantalainen@gmail.com>
2020-06-26 17:23:52 +0300

    created functionalities for therapy screen including different therapy sections

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="home-page col-12">
-            <div style={{position: "absolute", bottom: 0}}>Home page</div>
+            <div></div>
         </div>
     )
 }
--- client/src/components/pages/Therapy.jsx
+++ client/src/components/pages/Therapy.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { therapies } from '../../assets/therapies';
 
 const Therapy = () => {
+    const [currentSelection, setCurrentSelection] = useState('yksilo');
+
+    const TherapyNaviItem = props => {
+        return (
+            <li className="col-4 float-left">
+                <span id={props.id} onClick={(e) => {setCurrentSelection(e.target.id)}}>{props.title}</span>
+            </li>
+        )
+    }
+
     return (
     <div className="therapy-page col-12">
-        <div style={{position: "absolute", bottom: 0}}>Therapy page</div>
+        <div className="font-basic p-md-5 p-1">
+            {
+                currentSelection === 'yksilo' ? (
+                    <span>{therapies.yksilo}</span>
+                ) : currentSelection === 'pari' ? (
+                    <span>{therapies.pari}</span>
+                ) : currentSelection === 'perhe' ? (
+                    <span>{therapies.perhe}</span>
+                ) : (
+                    null
+                )
+            }
+        </div>
+        <div className="therapy-nav">
+            <ul className="col-12 row">
+                <TherapyNaviItem id={"yksilo"} title={"Yksilö"} />
+                <TherapyNaviItem id={"pari"} title={"Pari"} />
+                <TherapyNaviItem id={"perhe"} title={"Perhe"} />
+            </ul>
+        </div>
     </div>
     )
 }
